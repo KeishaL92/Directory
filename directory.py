280c1cd614935aedeaef943a7bb4ff336681994a
KeishaL92 <105615586+KeishaL92@users.noreply.github.com>
2022-05-22 22:02:12 -0500

    Add files via upload

---
+++ directory.py
@@ -0,0 +1,15 @@
+import os
+
+
+file_path = '/keish/OneDrive/Desktop/Bellevue/INTRO TO PROGRAMMING'
+with open(file_path) as file_object
+for line in file_object:
+	print(line)
+directory = input("Please input desired directory: ")
+filename = input("Please input desired file name: ")
+user_Name = input("What is your name? ")
+user_Address = input("What is your address? ")
+user_Phone = input("What is your phone number? ")
+
+with open(directory, filename, 'w')
+print(user_Name ',' user_Address ',' user_Phone)
